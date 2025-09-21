8c40e6ddf26edf73534a8592c2fad1ce305b91a0
Jafar <jafar.maratov@gmail.com>
2025-09-21 13:48:31 +0300

    Факультатив

---
+++ fak.py
@@ -0,0 +1,11 @@
+a = [1,2,3,4,5,6,7,8,9]
+
+def al(A):
+    for v in A:
+        for x in A:
+            if v < x:
+                break
+        else:
+            return v
+    return None
+print(al(a))
