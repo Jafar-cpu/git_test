0d5eb10681aba2008eebc9a752c12b8cb8038add
Jafar <jafar.maratov@gmail.com>
2025-09-26 16:59:15 +0300

    соревы

--- fak.py
+++ fak.py
@@ -1,12 +1,17 @@
-a = [1,2,3,4,5,6,7,8,9]
+def largest_two(a):
+    my_max = a[0]
+    my_max2 = a[1]
+    if my_max < my_max2:
+        my_max,my_max2 = my_max2,my_max
+        
+    for i in range(1,len(a)):
+        if my_max < a[i]:
+            my_max,my_max2 = a[i],my_max
+        elif my_max2 < a[i]:
+            my_max2 = a[i]
+        
+    return my_max,my_max2
+        
 
-
-def al(A):
-    for v in A:
-        for x in A:
-            if v < x:
-                break
-        else:
-            return v
-    return None
-print(al(a))
+a = [9,6,7,8,4,5,3,2,1]
+print(None * 12)
